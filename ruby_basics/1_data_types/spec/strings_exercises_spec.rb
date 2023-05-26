5badff5f8753a17025a241ec55e7261a92b13b1c
Motlakz <motlalepulasello5@gmail.com>
2023-05-26 15:10:20 +0200

    practicals: finish string exercises

--- ruby_basics/1_data_types/spec/strings_exercises_spec.rb
+++ ruby_basics/1_data_types/spec/strings_exercises_spec.rb
@@ -16,97 +16,180 @@ RSpec.describe 'String Exercises' do
       expect(concatenate('world')).to eq('Hello world!')
     end
 
-    xit 'returns "Hello universe!"' do
+    def concatenate(str)
+      return "Hello #{str}!"
+    end    
+
+    it 'returns "Hello universe!"' do
       expect(concatenate('universe')).to eq('Hello universe!')
     end
+
+    def concatenate(str2)
+      return "Hello #{str2}!"
+    end
+    
   end
 
   describe 'substrings exercise' do
 
-    xit 'returns the first 4 letters of the word' do
+    it 'returns the first 4 letters of the word' do
       expect(substrings('chocolate')).to eq('choc')
     end
+
+    def substrings(chocolate)
+      return "chocolate"[0, 4]
+    end
+    
   end
 
   describe 'capitalizing exercise' do
 
-    xit 'capitalizes a word' do
+    it 'capitalizes a word' do
       expect(capitalize('paris')).to eq('Paris')
     end
 
-    xit 'only capitalizes the first word if there are multiple words' do
+    def capitalize(word)
+      word.capitalize
+    end
+
+    it 'only capitalizes the first word if there are multiple words' do
       expect(capitalize('miami in the summer')).to eq('Miami in the summer')
     end
 
-    xit 'leaves an already capitalized word as is' do
+    def cap(words_in_a_sentence)
+      words_in_a_sentence = 'miami in the summer'
+      words_in_a_sentence.capitalize
+    end
+
+    it 'leaves an already capitalized word as is' do
       expect(capitalize('London')).to eq('London')
+    end
+
+    def caps(word)
+      word = 'London'
+      return word
     end
   end
 
   describe 'uppercasing exercise' do
 
-    xit 'uppercases a word' do
+    it 'uppercases a word' do
       expect(uppercase('small')).to eq('SMALL')
     end
 
-    xit 'uppercases multiple words' do
+    def uppercase(word)
+      return word.upcase
+    end
+
+    it 'uppercases multiple words' do
       expect(uppercase('make me bigger')).to eq('MAKE ME BIGGER')
+    end
+
+    def titalize(words)
+      return words.titalize
     end
   end
 
 
   describe 'downcasing exercise' do
 
-    xit 'downcases a word' do
+    it 'downcases a word' do
       expect(downcase('LARGE')).to eq('large')
     end
 
-    xit 'downcases multiple words' do
+    def downcase(text)
+      return text.downcase
+    end
+
+    it 'downcases multiple words' do
       expect(downcase('MAKE ME SMALLER')).to eq('make me smaller')
+    end
+
+    def downsize(words)
+      words = 'A BUNCH OF WORDS'
+      return words.downcase
     end
   end
 
   describe 'empty exercise' do
 
-    xit 'returns true if string is empty' do
+    it 'returns true if string is empty' do
       expect(empty_string('')).to eq(true)
     end
 
-    xit 'returns false if string is not empty' do
-      expect(empty_string('wow')).to eq(false)
+    def empty_string(str)
+      str = ''
+      str.empty?
+    end
+
+    it 'returns false if string is not empty' do
+      expect(string_is_empty('wow')).to eq(false)
+    end
+
+    def string_is_empty(string)
+      string = 'word'
+      string.empty?
     end
   end
 
   describe 'length exercise' do
 
-    xit 'returns the length of a word' do
+    it 'returns the length of a word' do
       expect(string_length('longitude')).to eq(9)
     end
 
-    xit 'returns the length of a string with multiple words' do
-      expect(string_length('i am quite long')).to eq(15)
+    def string_length(word)
+      word = 'longitude'
+      word.length
+    end
+
+    it 'returns the length of a string with multiple words' do
+      expect(multiple_length('i am quite long')).to eq(15)
+    end
+
+    def multiple_length(sentence)
+      sentence = 'I am quite long'
+      sentence.length
     end
   end
 
   describe 'reverse exercise' do
 
-    xit 'reverses a word' do
+    it 'reverses a word' do
       expect(reverse('desrever')).to eq('reversed')
     end
 
-    xit 'reverses multiple words' do
-      expect(reverse('dnuora kcab')).to eq('back around')
+    def reverse(word)
+      word = 'desrever'
+      word.reverse
+    end
+
+    it 'reverses multiple words' do
+      expect(revert('dnuora kcab')).to eq('back around')
+    end
+
+    def revert(words)
+      words = 'dnuora kcab'
+      words.reverse
     end
   end
 
   describe 'remove spaces exercise' do
 
-    xit 'removes a single space' do
+    it 'removes a single space' do
       expect(space_remover('white space')).to eq('whitespace')
     end
 
-    xit 'removes multiple spaces' do
-      expect(space_remover('many white spaces')).to eq('manywhitespaces')
+    def space_remover(space)
+      space.gsub(' ', '')
+    end
+
+    it 'removes multiple spaces' do
+      expect(space_removal('many white spaces')).to eq('manywhitespaces')
+    end
+
+    def space_removal(spaces)
+      spaces.gsub(' ', '')
     end
   end
 end
